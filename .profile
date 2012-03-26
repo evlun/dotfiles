0b1be0d8a1797aae502d83912fe63148f39afbb5
Erik Lundin <mjor.himself@gmail.com>
2012-03-26 11:11:52 +0200

    Add .profile

---
+++ .profile
@@ -0,0 +1,4 @@
+# Sublime Text 2
+export PATH=/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/:$PATH
+export EDITOR='subl -w'
+export VISUAL='subl -w'
---
+++ Development/XCode/square/square/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  square
+//
+//  Created by Erik Lundin on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
